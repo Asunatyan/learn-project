8ea2633a88c3b468c9b2f45d3a406078a344c0ea
dingqiaokun <qiaokun.d@wingto.cn>
2020-05-09 17:37:41 +0800

    初始化

---
+++ study-2020/src/test/java/com/dqk/study/study2020/Study2020ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dqk.study.study2020;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Study2020ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
